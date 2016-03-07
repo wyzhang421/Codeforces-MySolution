892a3e0e7b5fe5ba05c66c03f5f90e2e20b29e7f
ZHANG Wanying <zhangwanying421@gmail.com>
2016-03-07 22:52:53 +0800

    1A

---
+++ 1A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main(){
+	int n, m, a;
+	
+	cin >> n >> m >> a;
+	
+	int w = n%a == 0 ? n/a : n/a+1;
+	int h = m%a == 0 ? m/a : m/a+1;
+	
+	cout << (long long)w*h << endl; 
+	
+	return 0;
+	
+}
+
